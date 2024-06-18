f23717c9b3a5e72d59a91c1fb2d020757e52374f
yrkims <kyl7809@gmail.com>
2024-06-18 17:56:38 +0900

    added swiper

--- js/comm.js
+++ js/comm.js
@@ -2,3 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector('.intro1').classList.add('animate');
             document.querySelector('.intro2').classList.add('animate');
 });
+
+
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+      direction: "vertical",
+      slidesPerView: 1,
+      spaceBetween: 30,
+      mousewheel: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+ 
